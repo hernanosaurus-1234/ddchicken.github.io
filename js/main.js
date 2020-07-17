26994500dda24786ae2f2891d73fc7e00ce9426e
hernanosaurus <hernanterania@gmail.com>
2020-07-17 17:18:17 +0800

    Updated overall design and functionality
    Updated overall design and functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,196 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+
+// /*
+
+// 	Featured Items
+
+// */
+
+// gsap.set(".item", {y: 100, autoAlpha: 0})
+
+// gsap.to("#item-1", {
+// 	scrollTrigger: {
+// 		trigger       : "#hero",
+// 		toggleActions : "play none none none",
+// 		start         : "50% 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	y         : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.5,
+// 	ease      : "power1.out",
+// 	z         : 0.01,
+// });
+
+// gsap.to("#item-2", {
+// 	scrollTrigger: {
+// 		trigger       : "#hero",
+// 		toggleActions : "play none none none",
+// 		start         : "50% 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	y         : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.5,
+// 	ease      : "power1.out",
+// 	z         : 0.01,
+// 	delay     : 0.5,
+// });
+
+// gsap.to("#item-3", {
+// 	scrollTrigger: {
+// 		trigger       : "#hero",
+// 		toggleActions : "play none none none",
+// 		start         : "50% 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	y         : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.5,
+// 	ease      : "power1.out",
+// 	z         : 0.01,
+// 	delay     : 1.0,
+// });
+
+// /*
+
+// 	Order Now
+
+// */
+
+// gsap.set("#order .center", {y: 100, autoAlpha: 0})
+
+// gsap.to("#order .center", {
+// 	scrollTrigger: {
+// 		trigger       : ".item-grid",
+// 		toggleActions : "play none none none",
+// 		start         : "50% 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	y         : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.75,
+// 	ease      : "power1.out",
+// 	delay     : 0.25,
+// 	z         : 0.01,
+// });
+
+// /*
+
+// 	Split Container
+
+// */
+
+
+// gsap.set(".split-box", { width: 0, autoAlpha: 0 })
+// gsap.set(".split-box .title", { x: -100, autoAlpha: 0 })
+// gsap.set(".split-box .detail", { x: -100, autoAlpha: 0 })
+
+// gsap.to("#delivery-box", {
+// 	scrollTrigger: {
+// 		trigger       : "#order",
+// 		toggleActions : "play none none none",
+// 		start         : "0 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	width     : '100%',
+// 	x 		  : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.75,
+// 	ease      : "power1.out",
+// 	delay     : 0.25,
+// 	z         : 0.01,
+// });
+
+// gsap.to("#delivery-box .title", {
+// 	scrollTrigger: {
+// 		trigger       : "#order",
+// 		toggleActions : "play none none none",
+// 		start         : "50% 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	x 		  : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.75,
+// 	ease      : "power1.out",
+// 	delay     : 0.75,
+// 	z         : 0.01,
+// });
+
+// gsap.to("#delivery-box .detail", {
+// 	scrollTrigger: {
+// 		trigger       : "#order",
+// 		toggleActions : "play none none none",
+// 		start         : "50% 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	x 		  : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.75,
+// 	ease      : "power1.out",
+// 	delay     : 0.75,
+// 	z         : 0.01,
+// });
+
+
+// gsap.to("#signup-box", {
+// 	scrollTrigger: {
+// 		trigger       : "#order",
+// 		toggleActions : "play none none none",
+// 		start         : "0 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	width     : '100%',
+// 	x 		  : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.75,
+// 	ease      : "power1.out",
+// 	delay     : 0.75,
+// 	z         : 0.01,
+// });
+
+// gsap.to("#signup-box .title", {
+// 	scrollTrigger: {
+// 		trigger       : "#order",
+// 		toggleActions : "play none none none",
+// 		start         : "50% 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	x 		  : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.75,
+// 	ease      : "power1.out",
+// 	delay     : 1.5,
+// 	z         : 0.01,
+// });
+
+// gsap.to("#signup-box .detail", {
+// 	scrollTrigger: {
+// 		trigger       : "#order",
+// 		toggleActions : "play none none none",
+// 		start         : "50% 50%",
+// 		// markers       : true,
+// 	},
+// 	// Animation
+// 	x 		  : 0,
+// 	autoAlpha : 1,
+// 	duration  : 0.75,
+// 	ease      : "power1.out",
+// 	delay     : 1.25,
+// 	z         : 0.01,
+// });
+
+
 const menu = (function() {
 	let isOpen = false;
 
@@ -8,14 +201,7 @@ const menu = (function() {
 
 				gsap.to($('#mobile-nav'), {
 					duration  : 0.6,
-					left      : '0%',
-					ease      : 'expo.out',
-					overwrite : true
-				});
-				gsap.to($('#mobile-nav .nav-wrapper'), {
-					duration  : 1.2,
-					right     : '0%',
-					delay     : 0.1,
+					top     : '0%',
 					ease      : 'expo.out',
 					overwrite : true
 				});
@@ -23,26 +209,20 @@ const menu = (function() {
 				$('#hamburger .hamburger').addClass('is-active');
 				$('#hamburger').addClass('menu-active');
 
-				$('body').addClass('stop-scrolling');
+				// $('body').addClass('stop-scrolling');
 			} else {
 				isOpen = false;
 
-				gsap.to($('#mobile-nav .nav-wrapper'), {
-					duration : 0.6,
-					right    : '-120%',
-					ease     : 'expo.out'
-				});
 				gsap.to($('#mobile-nav'), {
-					duration : 1.2,
-					left     : '120%',
-					delay    : 0.1,
+					duration : 0.6,
+					top     : '-100%',
 					ease     : 'expo.out'
 				});
 
 				$('#hamburger .hamburger').removeClass('is-active');
 				$('#hamburger').removeClass('menu-active');
 
-				$('body').removeClass('stop-scrolling');
+				// $('body').removeClass('stop-scrolling');
 			}
 		});
 	}
